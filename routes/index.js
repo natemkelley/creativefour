15dacf191fc07ce52dd54dc18bad74e27f873440
Nate <natemkelley@gmail.com>
2017-11-03 21:34:09 +0000

    swag

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,10 @@ var request = require('request')
 var fs = require('fs');
 const https = require('https');
 
+
+var highscore = 100;
+
+
 /* GET home page. */
 router.get('/', function (req, res, next) {
     res.sendFile('index.html', {
@@ -19,8 +23,22 @@ router.get('/getquestion', function (req, res, next) {
     request(myWord).pipe(res);
 });
 
+router.get('/highscore', function (req, res, next) {
+    console.log("In highscore");
+    res.send(highscore.toString());
+});
+
 router.post('/getanswer', function (req, res, next) {
     //var userinput = req.query.q.toLowerCase();
+    var score = req.query.s;
+    console.log(score);
+
+    if (score > highscore) {
+        highscore = score;
+        res.end('{"success":"You have the high score!" , "status" : 200}');
+
+    }
+
     res.end('{"success":"correct", "status" : 200}');
 
 });
